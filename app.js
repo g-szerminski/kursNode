638be43e8b5499e9fefda561759d5b395c2ad515
Grzegorz Szermiński <szerman@poczta.fm>
2018-10-01 12:57:08 +0200

    WIP

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-var bufer = new Buffer('Bufor');
-bufer.write('abc');
-console.log(bufer.toString());
+var fs = require('fs');
 
-var bufer = new Buffer('Bufor');
-console.log(bufer[0]);
-
-var bufer = new Buffer('Bufor');
-console.log(bufer.toJSON());
-
-var bufer = new Buffer('Bufor');
-console.log(bufer.toString());
+var readable = fs.createReadStream('./data.txt')
