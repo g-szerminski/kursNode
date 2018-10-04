bd663e1f3255d29218c5c19e4f23b63eeddd4fdd
Grzegorz Szermiński <szerman@poczta.fm>
2018-10-04 16:36:18 +0200

    Add lesson 6-5

---
+++ app.js
@@ -0,0 +1,18 @@
+var http = require('http');
+http.createServer(function(req, res){
+if(req.url === '/') {
+	res.writeHead(200);
+	res.end('Strona startowa');
+} else if (req.url === '/products') {
+	res.writeHead(200);
+	res.end('Strona Produktu');
+} else if (req.url === '/blog') {
+	res.writeHead(200);
+	res.end('Strona Blog');
+}	else {
+	res.writeHead(404);
+	res.end('Strony nie znaleziono');
+}
+}).listen(3000);
+
+console.log('Serwer uruchomiony...');
