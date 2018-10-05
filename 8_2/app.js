4a7da86a78dccca4c6703b68c0d63d95c4b2932c
Grzegorz Szermiński <szerman@poczta.fm>
2018-10-05 19:52:40 +0200

    Add lesson 8-2 - tamporary

---
+++ 8_2/app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+
+var app = express();
+
+/* Routing */
+app.get('/', function(req, res){
+	res.send('Strona Start');
+});
+
+app.get('/contact', function(req, res){
+	res.send('Strona Kontakt');
+});
+
+app.get('/product/:id', function(req, res){
+	res.send('Wartość ID: ' + req.params.id);
+});
+
+app.listen(3000);
