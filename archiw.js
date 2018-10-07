1344af533e824e74a9df4836e8907c5b6772d160
Grzegorz Szermiński <szerman@poczta.fm>
2018-10-07 18:34:34 +0200

    Add lesson 8-2

--- archiw.js
+++ archiw.js
@@ -1,3 +1,20 @@
+//video 8-2
+var express = require('express');
+var app = express();
+
+/* Routing */
+app.get('/', function(req, res){
+	res.send('Strona Start');
+});
+app.get('/contact', function(req, res){
+	res.send('Strona Kontakt');
+});
+app.get('/product/:id', function(req, res){
+	res.send('Wartość ID: ' + req.params.id);
+});
+app.listen(3000);
+
+
 //video 8-1
 var express = require('express');
 var app = express();
