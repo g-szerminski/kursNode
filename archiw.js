4f12a3909149bc007973d6e10a43c4b1dc2bfadc
Grzegorz Szermiński <szerman@poczta.fm>
2018-10-10 11:14:27 +0200

    Add lesson 8-6

--- archiw.js
+++ archiw.js
@@ -1,3 +1,41 @@
+//video 8-6
+
+//video 8-5
+var express = require('express');
+var app = express();
+app.set('view engine','jade');
+app.use('/assets', express.static('public'));
+
+/* Routing */
+app.get('/', function(req, res){
+	res.render('index', { query: req.query.id })
+});
+app.get('/contact', function(req, res){
+	res.send('Strona Kontakt');
+});
+app.get('/product/:id', function(req, res){
+	res.send('Wartość ID: ' + req.params.id);
+});
+app.listen(3000);
+
+//video 8-4
+var express = require('express');
+var app = express();
+app.set('view engine','jade');
+app.use('/assets', express.static('public'));
+
+/* Routing */
+app.get('/', function(req, res){
+	res.render('index', { header: 'Witaj na stronie Index'})
+});
+app.get('/contact', function(req, res){
+	res.send('Strona Kontakt');
+});
+app.get('/product/:id', function(req, res){
+	res.send('Wartość ID: ' + req.params.id);
+});
+app.listen(3000);
+
 //video 8-3
 var express = require('express');
 var app = express();
